5dd032221f05392374778a4ff00327e721c3e4a7
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-11-20 14:17:14 +0200

    Add project files.

---
+++ Expander/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Expander
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            productListBox.ItemsSource = products;
+        }
+
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            string productName = productNameTextBox.Text;
+            string productInfo = productInfoTextBox.Text;
+
+            if (!string.IsNullOrEmpty(productName))
+            {
+                products.Add(new Product { Name = productName, AdditionalInfo = productInfo });
+                productNameTextBox.Clear();
+                productInfoTextBox.Clear();
+            }
+        }
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button deleteButton = (Button)sender;
+            Product product = (Product)deleteButton.DataContext;
+
+            products.Remove(product);
+        }
+    }
+    public class Product
+    {
+        public string? Name { get; set; }
+        public string? AdditionalInfo { get; set; }
+    }
+}
---
+++ HW2_DownloadManager/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HW2_DownloadManager
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private async void downloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(urlTextBox.Text))
+            {
+                try
+                {
+                    await DownloadFileAsync(urlTextBox.Text);
+                    MessageBox.Show("Downlad completed!");
+                }
+                catch (Exception ex) { MessageBox.Show($"Error: {ex.Message}"); }
+            }
+            else { MessageBox.Show("Please enter a valid URL."); }
+        }
+        private async Task DownloadFileAsync(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                Uri uri = new Uri(url);
+                string fileName = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), System.IO.Path.GetFileName(uri.LocalPath));
+
+                client.DownloadProgressChanged += (s, e) => downloadProgressBar.Value = e.ProgressPercentage;
+
+                await client.DownloadFileTaskAsync(uri, fileName);
+            }
+        }
+    }
+}
